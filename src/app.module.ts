1aa1fe547007c0d3f133b5a890409359870d5aeb
roqurdl <uyhnri@gmail.com>
2022-11-29 22:18:31 +0900

    try to connect postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '172.25.192.1',
+      port: 5432,
+      username: 'rolph',
+      password: `381858`,
+      database: 'number-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
