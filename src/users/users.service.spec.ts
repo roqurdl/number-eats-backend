9dc394765584c69107e033e6ade248e13f2c1cad
roqurdl <uyhnri@gmail.com>
2022-12-23 19:24:33 +0900

    userCreate test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,17 +1,17 @@
 import { Test } from '@nestjs/testing';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { EmailService } from 'src/email/email.service';
 import { JwtService } from 'src/jwt/jwt.service';
 import { Users } from './entities/users.entity';
 import { Verification } from './entities/verification.entity';
 import { UserService } from './users.service';
+import { EmailService } from 'src/email/email.service';
 
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 const mockJwtService = {
   sign: jest.fn(),
   verify: jest.fn(),
@@ -25,6 +25,8 @@ type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 describe(`UserService`, () => {
   let service: UserService;
   let usersRepository: MockRepository<Users>;
+  let verfiRepository: MockRepository<Verification>;
+  let emailService: EmailService;
 
   beforeAll(async () => {
     const modules = await Test.createTestingModule({
@@ -32,11 +34,11 @@ describe(`UserService`, () => {
         UserService,
         {
           provide: getRepositoryToken(Users),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verification),
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService,
@@ -49,7 +51,9 @@ describe(`UserService`, () => {
       ],
     }).compile();
     service = modules.get<UserService>(UserService);
+    emailService = modules.get<EmailService>(EmailService);
     usersRepository = modules.get(getRepositoryToken(Users));
+    verfiRepository = modules.get(getRepositoryToken(Verification));
   });
 
   it(`should be defined`, () => {
@@ -60,17 +64,54 @@ describe(`UserService`, () => {
     it('should fail if user exists', async () => {
       usersRepository.findOne.mockResolvedValue({
         id: 1,
-        email: '',
+        email: `test@please.work`,
       });
-      const result = await service.createAccount({
-        email: '',
-        password: '',
-        role: 0,
-      });
+      const result = await service.createAccount(createAccountArgs);
       expect(result).toMatchObject({
         ok: false,
-        error: 'This email already used.',
+        error: `This email already used.`,
       });
+    });
+
+    const createAccountArgs = {
+      email: 'test@email.com',
+      password: 'test.password',
+      role: 0,
+    };
+
+    it(`should creat new user`, async () => {
+      usersRepository.findOne.mockResolvedValue(undefined);
+
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+
+      verfiRepository.create.mockReturnValue({ user: createAccountArgs });
+      verfiRepository.save.mockResolvedValue({ code: `test` });
+
+      const result = await service.createAccount(createAccountArgs);
+
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(usersRepository.save).toHaveBeenCalled();
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(verfiRepository.create).toHaveBeenCalledTimes(1);
+      expect(verfiRepository.create).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(verfiRepository.save).toHaveBeenCalled();
+      expect(verfiRepository.save).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(emailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(emailService.sendVerificationEmail).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.any(String),
+      );
+      expect(result).toEqual({ ok: true });
     });
   });
 
